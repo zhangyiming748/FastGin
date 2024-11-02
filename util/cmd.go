26d58a78565a8e0905b520f47d51355155005b2c
zen <mba>
2024-11-03 00:15:37 +0800

    复用下载逻辑

--- util/cmd.go
+++ util/cmd.go
@@ -82,3 +82,36 @@ func GetKey(s string) string {
 		return ""
 	}
 }
+
+/*
+执行tdl命令过程中可以循环打印消息
+*/
+func ExecTdlCommand(tdl, proxy, uri, target string) (e error) {
+	c := exec.Command(tdl, "download", "--proxy", proxy, "--threads", "8", "--url", uri, "--dir", target)
+	log.Printf("开始执行命令:%v\n", c.String())
+	stdout, err := c.StdoutPipe()
+	c.Stderr = c.Stdout
+	if err != nil {
+		log.Printf("连接Stdout产生错误:%v\n", err)
+		return err
+	}
+	if err = c.Start(); err != nil {
+		log.Printf("启动cmd命令产生错误:%v\n", err)
+		return err
+	}
+	for {
+		tmp := make([]byte, 1024)
+		_, err := stdout.Read(tmp)
+		t := string(tmp)
+		t = strings.Replace(t, "\u0000", "", -1)
+		fmt.Printf("\r%v", t)
+		if err != nil {
+			break
+		}
+	}
+	if err = c.Wait(); err != nil {
+		log.Printf("命令执行中产生错误:%v\n", err)
+		return err
+	}
+	return nil
+}
